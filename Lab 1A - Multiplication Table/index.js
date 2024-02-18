0d0c7985c0d115f809c3b85ca8768e529bb5bf5c
cryptopig <vpig76@gmail.com>
2024-02-17 22:09:22 -0800

    s

--- Lab 1A - Multiplication Table/index.js
+++ Lab 1A - Multiplication Table/index.js
@@ -1,7 +1,25 @@
-var color = document.getElementById('colors').onclick = changeColors;
+var color = document.getElementById('colors');
 var swatch = document.getElementById('swatch');
+color.onclick = changeColors;
 
 function changeColors(e) {
     console.log(e.target.textContent);
     swatch.style.backgroundColor = e.target.textContent;
 }
+
+function makeMultiplicationTable(rows, columns) {
+    var tableContent;
+    tableContent += `<th> ${rows}x${columns} Multiplication Table </th>` 
+    // I also learned about "template literals" which get rid of a lot of plus signs by using ``.
+    for (var y = 1; y <= rows; y++ ) {
+        tableContent += "<tr>";
+        for (var x = 1; x <= columns; x++) {
+            tableContent += "<td>" + (x*y) + "</td>";
+        }
+        tableContent += "</tr>\n";
+    }  
+    return tableContent;
+}
+var multiplicationTable = makeMultiplicationTable(12, 12);
+
+window.onload (document.getElementById('mtable').innerHTML = multiplicationTable);
