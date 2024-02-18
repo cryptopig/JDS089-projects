f139a5891c7d05490cf8f1a1226e5e8b0cd9986b
cryptopig <vpig76@gmail.com>
2024-02-17 17:59:13 -0800

    s

--- Multiplication Table/index.js
+++ Multiplication Table/index.js
@@ -1,5 +1,5 @@
-.h1
-{
-    alignment: 'center';
-    
+var color = Document.getElementById('colors').onclick = changeColors();
+var swatch = Document.getElementById('swatch');
+function changeColors(e) {
+    console.log(e);
 }
