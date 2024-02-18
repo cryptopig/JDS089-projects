acc0828787c15e063cc285355cebabeb01e96439
cryptopig <vpig76@gmail.com>
2024-02-17 20:05:56 -0800

    lab 3b

--- Multiplication Table/index.js
+++ Multiplication Table/index.js
@@ -1,5 +1,6 @@
-var color = Document.getElementById('colors').onclick = changeColors();
-var swatch = Document.getElementById('swatch');
+var color = document.getElementById("colors").onclick = changeColors;
+var swatch = document.getElementById("swatch");
+
 function changeColors(e) {
     console.log(e.target.textContent);
     swatch.style.backgroundColor = e.target.textContent;
