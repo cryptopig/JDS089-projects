4b9d26384df96e60281af59fed7c802e7e5e6b75
cryptopig <71409503+cryptopig@users.noreply.github.com>
2024-04-11 00:45:44 -0700

    4A done!

--- Lab 4A - Templates/index.js
+++ Lab 4A - Templates/index.js
@@ -1,40 +1,77 @@
-const fs = require('fs');
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
 
 http.createServer((req, res) => {
-    // for debugging
+    let header = "";
+    let content = "";
+    let footer = "";
+
     console.log(__dirname, req.url);
 
-    // routes
-    if (req.url == '/' || req.url == '/home') {
-        req.url = '/index.html';
+    if (req.url == "/") {
+        req.url = "/index.html";
     }
-    
-    
-    // header
-    fs.readFile("header.inc", (err, data) => {
-        res.writeHead(200, { 'Content-Type': 'text/html'});
-        res.write(data);
-        
-    });
-    
-    fs.readFile(__dirname + req.url.replace('.html', '.inc'), (err, data) => {
-       if (err) {
-           res.writeHead(404, { 'Content-Type': 'text/html'});
-           res.end('404: File not found.');
-       } else {
-           res.writeHead(200, {'Content-Type': 'text/html'});
-           res.writeHead(200, {'Access-Control-Allow-Origin': '*'});
-           res.write(data);
-           
-       }
-        
-    });
-    
-    // footer
-    fs.readFile("footer.inc", (err, data) => {
-        res.writeHead(200, { 'Content-Type': 'text/html'});
-        res.end(data); // end of data, so it should be res.end(data)
 
+    // header
+    fs.readFile("header.html", (err, data) => {
+        if (err) {
+            handleError(res, err);
+            return;
+        }
+        header = data;
+        writeContent();
     });
-}).listen(8080);
+
+    // footer
+    fs.readFile("footer.html", (err, data) => {
+        if (err) {
+            handleError(res, err);
+            return;
+        }
+        footer = data;
+        writeContent();
+    });
+
+    // content
+    if (req.url.endsWith(".html")) {
+        fs.readFile(__dirname + req.url.replace(".html", ".html"), (err, data) => {
+            if (err) {
+                handleError(res, err);
+                return;
+            }
+            content = data;
+            writeContent();
+        });
+    } else {
+        // Non-HTML files
+        fs.readFile(__dirname + req.url, (err, data) => {
+            if (err) {
+                handleError(res, err);
+                return;
+            }
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        });
+    }
+
+    function writeContent() {
+        // Check if all files have been read
+        if (header && content && footer) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(header);
+            res.write(content);
+            res.end(footer);
+        }
+    }
+
+    function handleError(res, err) {
+        console.error(err);
+        res.writeHead(500, {'Content-Type': 'text/html'});
+        res.end('500: Internal Server Error');
+    }
+}).listen(PORT);
+
+console.log(`Server running on port ${PORT}`);
